edae803ecc770b26674eee60ad38e4a58ad8088c
zpk <zpk>
2026-01-24 12:44:11 +0800

    updates theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -8,12 +8,28 @@ return {
   -- 安装 Kanagawa 主题 (强烈推荐)
   { "rebelot/kanagawa.nvim", priority = 1000 },
 
+  -- 安装 tokyonight 主题
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "moon", -- 这里改风格: "storm", "moon", "night", "day"
+      transparent = true, -- 开启透明背景 (很酷！)
+      styles = {
+        sidebars = "transparent", -- 侧边栏也透明
+        floats = "transparent", -- 悬浮窗也透明
+      },
+    },
+  },
+
 
   -- 2. 配置 LazyVim 使用该主题
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      -- colorscheme = "gruvbox",
+      colorscheme = "tokyonight",
       -- colorscheme = "catppuccin",
       -- colorscheme = "kanagawa",
     },
